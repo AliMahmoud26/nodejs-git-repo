1aea44ef00759a4a9ef1cf41915c1bcb2dbb0b0a
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-04-05 14:21:23 +0200

    Commiting the eventEmitter File

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,4 +1,17 @@
 const EventEmitter = require('events');
 
-const customEmitter = new EventEmitter();
+// Instances of the EventEmitter
+const myEmitter  = new EventEmitter();
+const customeEmitter = new EventEmitter();
 
+// myEmitter
+myEmitter.on('greet', (name) => {
+    console.log(`Hello ${name}`);
+})
+myEmitter.emit('greet', 'John')
+
+// customEmitter
+customeEmitter.on('response', (msg) => {
+    console.log(`${msg}`);
+})
+customeEmitter.emit('response', 'This is the response message on the previous emitter')
